51fa4ca435e6ebefc4ea4ef20debaeddc3ea8963
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2022-04-27 22:43:28 +0530

    Typescript

---
+++ TypeScript/whyts.ts
@@ -0,0 +1,17 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// let result = add(2, 5);
+// let sum = add('2', '5');
+
+// console.log('Result: ', result);
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+let result = add(2, 5);
+//let sum = add('2', '5');
+
+console.log('Result: ', result);
