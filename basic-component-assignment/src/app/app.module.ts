5e1e4335eba9da01ccbde26373fde4bf737d6f11
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2022-05-01 00:40:52 +0530

    success component

--- basic-component-assignment/src/app/app.module.ts
+++ basic-component-assignment/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, WarningAlertComponent],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
