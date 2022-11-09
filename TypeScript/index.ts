43c0b7b8f27504cc032424606dce49a1e4b692f9
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2022-11-09 14:01:11 +0530

    TypeScript: What, when and why

---
+++ TypeScript/index.ts
@@ -0,0 +1,6 @@
+/*
+  1. JavaScript is dynamically typed language
+  2. TypeScript is statically typed language (It's superset of JavaScript with some additional features)
+  3. Dynamically typing vs. Statically typing
+  4. Looslly typed language vs Strongly typed language
+*/
