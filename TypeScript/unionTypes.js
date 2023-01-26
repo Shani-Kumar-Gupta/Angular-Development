2adee401119559095de17864951a3820b1f1eba6
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-01-26 22:36:54 +0530

    🚀 TypeScript Union Type Annotation

---
+++ TypeScript/unionTypes.js
@@ -0,0 +1,4 @@
+// Union Type - Want to assign multiple values in a single variable in TS
+var courseData = 'React';
+courseData = 12345;
+console.log(courseData);
---
+++ TypeScript/unionTypes.ts
@@ -0,0 +1,7 @@
+// Union Type - Want to assign multiple values in a single variable in TS
+
+let courseData: string | number = 'React';
+
+courseData = 12345;
+
+console.log(courseData);
