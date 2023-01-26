3a99ca69ecf8e7c3226b6a5ba498a66ca6feabc6
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-01-26 22:58:37 +0530

    🚀 Type Aliases

---
+++ TypeScript/typeAliases.ts
@@ -0,0 +1,24 @@
+/* Type Aliases */
+
+type Person = {
+  myName: string;
+  age: number;
+};
+
+// let person: {
+//   myName: string;
+//   age: number;
+// };
+
+let person: Person;
+
+// let people: {
+//   myName: string;
+//   age: number;
+// }[];
+
+let people: Person[];
+
+/* Above you can see: we are having two variables with same Object Property Type Annotation - 
+  Do we have any solutions so that we can globaly define that and use anywhere */
+
