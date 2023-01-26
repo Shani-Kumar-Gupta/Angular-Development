bd3c3e5bd4bfe45d2d9f7a79fccdc51652d3e9c0
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-01-26 22:27:46 +0530

    🚀 Type Inference

---
+++ TypeScript/typeInference.ts
@@ -0,0 +1,9 @@
+// Type Inference
+
+let course = 'Angular - The Complete Guide'; // We have not mentioned any Type Annotation in this line
+
+// let's assign some other value to course variable
+// course = 2345;
+/* Will Get Error in the above line - 
+Because Typescript infer the same type which value you have assigned at the time of declaration
+*/
