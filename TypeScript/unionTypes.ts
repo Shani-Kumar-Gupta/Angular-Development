9a35d8c47b743029e7b62afcffdfd7f041745aad
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-01-26 22:38:51 +0530

    🚀 Union Types

--- TypeScript/unionTypes.ts
+++ TypeScript/unionTypes.ts
@@ -5,3 +5,8 @@ let courseData: string | number = 'React';
 courseData = 12345;
 
 console.log(courseData);
+
+let names: string | string[];
+
+names = "Shani K.";
+names = ["Shani K.", "Shani K. Gupta"];
