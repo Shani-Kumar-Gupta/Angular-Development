b03eb6bb608aa1a07686fdc9288ca3b58544e205
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-05-12 12:10:45 +0530

    Iterable Array converted to Observables - from

--- ngRxjs/src/app/app.component.ts
+++ ngRxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngRxjs';
+  /* Example 1 */
+  postArray = [
+    { title: 'Software Engineering', description: 'Software Engineering description' },
+    { title: 'Web Development', description: 'Web Development description' },
+    { title: 'Database', description: 'Database description' }
+  ]; // Iterable Array
+
+  postArrayObservable$ = from(this.postArray); // Converted into Observable
+
+  constructor() {
+    this.postArrayObservable$.subscribe({
+      next: (data) => console.log(data),
+      error: (err) => console.log(err),
+      complete: () => console.log('completed')
+    });
+  }
 }
