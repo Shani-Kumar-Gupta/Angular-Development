986d66be8fc130fc7a3b14d640fe3c6f9547218c
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-05-13 22:28:16 +0530

    unsubscribe the observables

--- ngRxjs/src/app/components/cancelling-observables/cancelling-observables.component.ts
+++ ngRxjs/src/app/components/cancelling-observables/cancelling-observables.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-cancelling-observables',
   templateUrl: './cancelling-observables.component.html',
   styleUrls: ['./cancelling-observables.component.css']
 })
-export class CancellingObservablesComponent {
+export class CancellingObservablesComponent implements OnInit {
+  timerSubscription!: Subscription;
+  constructor() { }
+  ngOnInit(){
+    const newObservables = interval(1000);
+    this.timerSubscription = newObservables.subscribe((data) => {
+      console.log(new Date().toLocaleTimeString() + ' ' + data);
+    })
+  }
+
+  cancelTimer() {
+    console.log("Cancel the observables");
+    this.timerSubscription.unsubscribe();
+  }
 
 }
