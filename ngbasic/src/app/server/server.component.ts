f7f8ffa12af3de158577db5558c073339c0ddba8
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-05-27 20:45:08 +0530

    New component created using ng g c command

---
+++ ngbasic/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
+  providers: []
+})
+export class ServerComponent {
+  constructor() {}
+}
