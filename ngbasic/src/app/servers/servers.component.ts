85527981780dc9c4b707946fea30d68b22793455
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-05-31 23:59:19 +0530

    @Component decorator properties

--- ngbasic/src/app/servers/servers.component.ts
+++ ngbasic/src/app/servers/servers.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // template: `<h1>Hello MY Server</h1>`,
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // styles: [`
+  //   h3 {
+  //     background-color: lightgreen;
+  //   }
+  //   `],
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
 
