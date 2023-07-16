9299192b80a2ff2053f36b8aeb5be0e6e8b97e3f
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-07-16 20:02:21 +0530

    What is Angular and Framework

--- ngService/src/app/app.component.ts
+++ ngService/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngService';
+  /* What and Why Services?? */
+  /* A service is a re-usable Typescript class that can be used in multiple component across your abgular application. */
 }
